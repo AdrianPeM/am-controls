1eeed404687fb583a69390665089f66986cb309d
adrian <adrianperezm.00@gmail.com>
2022-03-21 14:25:48 -0600

    chore: Install FontAwesome icons

---
+++ src/utils/fontAwesome.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTablet, faEnvelope, faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFacebook, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope as farFaEnvelope } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faTablet, faEnvelope, faTwitter, faFacebook, faYoutube, faInstagram, farFaEnvelope, faAngleDown)
