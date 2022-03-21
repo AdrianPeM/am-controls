e77076c4895a84579b03bf36a0f1353987ab3ba3
adrian <adrianperezm.00@gmail.com>
2022-03-21 14:59:22 -0600

    feat: TestComponents add Loading component

--- src/pages/TestComponents/index.js
+++ src/pages/TestComponents/index.js
@@ -1,4 +1,4 @@
-import { AwesomeAnimation, CharacterField, TextField } from 'components'
+import { AwesomeAnimation, CharacterField, Loading, TextField } from 'components'
 import React, { useState } from 'react'
 
 const TestComponents = () => {
@@ -11,6 +11,10 @@ const TestComponents = () => {
             <TextField type='password' label='Password' placeholder='Type password' value={pass} onChange={setPass}/>
             <p>Awesome Animation</p>
             <AwesomeAnimation hover size='2em' icon='angle-down' animation='downUp' />
+            <p>Loading</p>
+            <div style={{height: '40px', width: '10em'}}>
+                <Loading isLoading={true} />
+            </div>
         </div>
     )
 }
