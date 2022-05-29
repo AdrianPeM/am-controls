6dab110d6764a5f67eca3feaf2cd355d5f17ddb5
adrian <adrianperezm.00@gmail.com>
2022-05-29 11:26:00 -0600

    refactor: Separate video blob and objectURL

--- src/pages/QuickTest/VideoRecording.js
+++ src/pages/QuickTest/VideoRecording.js
@@ -9,7 +9,7 @@ const VideoRecorder = (props) => {
         <div className="recorder_container" style={{ display: show ? '' : 'none' }}>
             <div className="recorder_video_container">
                 <video ref={recorderVideoRef} autoPlay muted></video>
-                <div ref={recordingFlagRef} className={`recording_flag${recording ? ' recording':''}`}>
+                <div ref={recordingFlagRef} className={`recording_flag${recording ? ' recording' : ''}`}>
                     <div className='recording_flag__circle'></div>
                 </div>
                 {showCounter &&
@@ -30,25 +30,29 @@ const VideoRecorder = (props) => {
 }
 
 const VideoPreview = (props) => {
-    const { show, videoBlob, resetRecord, saveChanges } = props
+    const { show, previewSrc, resetRecord, saveChanges } = props
 
     return (
         <div className='recorder_container' style={{ display: show ? '' : 'none' }}>
             <div className="recorder_video_container">
-                <video controls muted src={videoBlob}></video>
+                <video controls muted src={previewSrc}></video>
             </div>
             <div id="btn_send_record" className="recorder__button" onClick={saveChanges}><p>Send Video</p></div>
             <div id="btn_reset_record" className="recorder__button" onClick={resetRecord}><p>Record again</p></div>
+            <a id="btn_reset_record" className="recorder__button" href={previewSrc} download="testing.webm">Download Video</a>
         </div>
     )
 }
 
 const wait = delayMS => new Promise(resolve => setTimeout(resolve, delayMS))
 
+
+
 const VideoRecording = () => {
     const [recording, setRecording] = useState(false)
     const [recorded, setRecorded] = useState(false)
-    const [videoBlob, setVideoBlob] = useState(null)
+    const [video, setVideo] = useState({blob: null, previewSrc: null})
+    // const [videoBlob, setVideoBlob] = useState(null)
     const [showCounter, setShowCounter] = useState(false)
     const [timeLeft, setTimeLeft] = useState(initialTime)
 
@@ -60,7 +64,7 @@ const VideoRecording = () => {
 
     const showTimer = () => {
         setShowCounter(true)
-        wait(initialTime*1000 - 100).then(() => setShowCounter(false))
+        wait(initialTime * 1000 - 100).then(() => setShowCounter(false))
     }
 
     const startRecording = () => {
@@ -74,26 +78,33 @@ const VideoRecording = () => {
             recorder.start()
 
             setRecording(true)
-            // recordingFlagRef.current.classList.add('recording')
 
             const stopped = new Promise((resolve, reject) => {
                 recorder.onstop = resolve
                 recorder.onerror = event => reject(event.name)
             })
 
-            const recorded = wait(limitTime*1000).then(stopRecording)
+            setTimeout(stopRecording, limitTime*1000);
+
+            // const recorded = wait(limitTime * 1000).then(stopRecording)
 
             return Promise.all([
                 stopped,
-                recorded
+                // recorded
             ])
                 .then(() => data)
                 .then(recordedChunks => {
+                    console.log(recordedChunks)
                     const recordedBlob = new Blob(recordedChunks, { type: "video/webm" })
-                    setVideoBlob(URL.createObjectURL(recordedBlob))
+                    // setVideoBlob(recordedBlob)
+                    setVideo({blob: recordedBlob, previewSrc: URL.createObjectURL(recordedBlob)})
 
                     console.log("Successfully recorded " + recordedBlob.size + " bytes of " +
                         recordedBlob.type + " media.")
+
+                    var blob = new Blob(recordedChunks, {
+                        type: "video/webm"
+                    });
                 })
                 .catch(e => { console.log(e) })
         }, initialTime * 1000)
@@ -103,9 +114,8 @@ const VideoRecording = () => {
     const stopRecording = () => {
         const recorder = recorderRef.current
         if (recorder.state == "recording") {
-            recorder.state == "recording" && recorder.stop()
+            recorder.stop()
             setRecording(false)
-            // recordingFlagRef.current.classList.remove('recording')
         }
         setRecorded(true)
     }
@@ -128,8 +138,8 @@ const VideoRecording = () => {
         if (showCounter) {
             intervalRef.current = setInterval(() => {
                 setTimeLeft(time => {
-                    const timeLeft = time-1
-                    if(timeLeft === 0 ) {
+                    const timeLeft = time - 1
+                    if (timeLeft === 0) {
                         clearInterval(intervalRef.current)
                         return initialTime
                     }
@@ -151,7 +161,7 @@ const VideoRecording = () => {
                 recorderRef.current = new MediaRecorder(streamRef.current)
 
                 return new Promise(resolve => recorderVideoRef.current.onplaying = resolve)
-            }).catch(e => { console.log(e) })
+            }).catch(e => { alert(e.message) })
     }, [])
 
 
@@ -159,7 +169,7 @@ const VideoRecording = () => {
         <div className="main_container">
             {!recording && <p>Se va a mostrar un texto leelo</p>}
             <VideoRecorder {...{ show: !recorded, recorderVideoRef, recording, recordingFlagRef, startRecording, stopRecording, showCounter, timeLeft }} />
-            <VideoPreview {...{ show: recorded, videoBlob, resetRecord, saveChanges }} />
+            <VideoPreview {...{ show: recorded, previewSrc: video.previewSrc, resetRecord, saveChanges }} />
         </div>
     )
 }
